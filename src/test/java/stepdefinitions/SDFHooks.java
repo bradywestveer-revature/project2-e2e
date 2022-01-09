a30878850df8a2e37d9fdc64255159ce23ea6f51
bradywestveer-revature <93550118+bradywestveer-revature@users.noreply.github.com>
2022-01-08 20:48:49 -0600

    fixed Before hook not creating both users, fixed Before hook causing error if user already exists

--- src/test/java/stepdefinitions/SDFHooks.java
+++ src/test/java/stepdefinitions/SDFHooks.java
@@ -2,33 +2,43 @@ package stepdefinitions;
 
 import io.cucumber.java.After;
 import io.cucumber.java.BeforeAll;
+import org.openqa.selenium.UnhandledAlertException;
 import poms.RegisterPOM;
 
 public class SDFHooks {
 	//runs once before everything
 	@BeforeAll
 	public static void setUp () {
-		RegisterPOM registerPOM = new RegisterPOM (DriverSingleton.getInstance ());
+		try {
+			RegisterPOM registerPOM = new RegisterPOM (DriverSingleton.getInstance ());
+			
+			DriverSingleton.getInstance ().get ("http://localhost:4200/register");
+			
+			registerPOM.firstNameInput ("John");
+			registerPOM.lastNameInput ("Smith");
+			registerPOM.emailInput ("johnsmith@example.com");
+			registerPOM.usernameInput ("johnsmith");
+			registerPOM.passwordInput ("password");
+			registerPOM.confirmPasswordInput ("password");
+			registerPOM.clickRegisterBtn ();
+			
+			registerPOM.waitForSuccessfulRegister ();
+			
+			DriverSingleton.getInstance ().get ("http://localhost:4200/register");
+			
+			registerPOM.firstNameInput ("Dr. Sarah");
+			registerPOM.lastNameInput ("Smith");
+			registerPOM.emailInput ("sarahsmith@example.com");
+			registerPOM.usernameInput ("sarahsmith");
+			registerPOM.passwordInput ("password");
+			registerPOM.confirmPasswordInput ("password");
+			registerPOM.clickRegisterBtn ();
+			
+			registerPOM.waitForSuccessfulRegister ();
+		}
 		
-		DriverSingleton.getInstance ().get ("http://localhost:4200/register");
-		
-		registerPOM.firstNameInput ("John");
-		registerPOM.lastNameInput ("Smith");
-		registerPOM.emailInput ("johnsmith@example.com");
-		registerPOM.usernameInput ("johnsmith");
-		registerPOM.passwordInput ("password");
-		registerPOM.confirmPasswordInput ("password");
-		registerPOM.clickRegisterBtn ();
-		
-		DriverSingleton.getInstance ().get ("http://localhost:4200/register");
-		
-		registerPOM.firstNameInput ("Dr. Sarah");
-		registerPOM.lastNameInput ("Smith");
-		registerPOM.emailInput ("sarahsmith@example.com");
-		registerPOM.usernameInput ("sarahsmith");
-		registerPOM.passwordInput ("password");
-		registerPOM.confirmPasswordInput ("password");
-		registerPOM.clickRegisterBtn ();
+		//if user already exists, do nothing
+		catch (UnhandledAlertException ignored) {}
 	}
 	
 	//runs after every scenario
